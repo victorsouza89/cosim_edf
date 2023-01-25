70d4421cd1bfe1db661f377a36f46a521c3fdbc3
Victor <victor@MacBook-Air-de-Victor.local>
2023-01-25 15:58:48 +0100

    finish_first_example_mac

--- examples_mac/very_simple_control.py
+++ examples_mac/very_simple_control.py
@@ -13,13 +13,17 @@ def simulate_custom_input(show_plot=True):
     step_size = 0.01
 
     # define reference parameters
-    ref_t = 1.0 - 0.5*step_size
+    ref_t = 1.0 - 0.5 * step_size
     ref_v = 1.0
     ref_0 = 0.0
 
     # controls
     without_control = lambda step_ref, passed_outputs = None : step_ref
     proportional_control = lambda step_ref, passed_outputs, K=1 : [K*(s - passed_outputs[-1]) for s in step_ref]
+    controls_functions = {
+        "without control" : without_control, 
+        "closed loop" : proportional_control
+    }
 
     # read the model description
     model_description = read_model_description(fmu_filename)
@@ -34,12 +38,11 @@ def simulate_custom_input(show_plot=True):
     vr_out1 = vrs['Out1']
 
     # initialize
-    controls_functions = [without_control, proportional_control]
-    fmus = []
-    unzipdirs = []
-    for i in range(len(controls_functions)):
+    fmus = {}
+    unzipdirs = {}
+    for i in controls_functions:
         #extract the FMU
-        unzipdirs.append(extract(fmu_filename))
+        unzipdirs[i] = extract(fmu_filename)
         fmu = FMU2Slave(guid=model_description.guid,
                         unzipDirectory=unzipdirs[i],
                         modelIdentifier=model_description.coSimulation.modelIdentifier,
@@ -51,13 +54,13 @@ def simulate_custom_input(show_plot=True):
         fmu.enterInitializationMode()
         fmu.exitInitializationMode()
 
-        fmus.append(fmu)
+        fmus[i] = fmu
         
     
     time = start_time # initial time
     rows = []  # list to record the results
-    passed_outputs = {i : [0.0] for i in range(len(controls_functions))} # list to record the outputs
-    passed_inputs = {i : [0.0] for i in range(len(controls_functions))} # list to record the inputs
+    passed_outputs = {i : [0.0] for i in controls_functions} # list to record the outputs
+    passed_inputs = {i : [0.0] for i in controls_functions} # list to record the inputs
 
     # simulation loop
     while time < stop_time:
@@ -65,7 +68,7 @@ def simulate_custom_input(show_plot=True):
         # NOTE: the FMU.get*() and FMU.set*() functions take lists of
         # value references as arguments and return lists of values
 
-        for i in range(len(controls_functions)):
+        for i in controls_functions:
             # get the control function
             control = controls_functions[i]
             step_ref = [ref_v if time > ref_t else ref_0]
@@ -80,7 +83,7 @@ def simulate_custom_input(show_plot=True):
         time += step_size
         row = [time]
 
-        for i in range(len(controls_functions)):
+        for i in controls_functions:
             # get the values for 'inputs' and 'outputs'
             inputs, outputs = fmus[i].getReal([vr_input, vr_out1])
 
@@ -97,20 +100,20 @@ def simulate_custom_input(show_plot=True):
         # append the results
         rows.append(tuple(row))
 
-    for fmu in fmus:
-        # terminate
+    # terminate
+    for fmu in fmus.values():
         fmu.terminate()
         fmu.freeInstance()
 
     # clean up
-    for unzipdir in unzipdirs:
+    for unzipdir in unzipdirs.values():
         shutil.rmtree(unzipdir)
         
     # convert the results to a structured NumPy array
     list_types = [('time', np.float64)]
-    for i in range(len(controls_functions)):
-        list_types.append(('input - control '+str(i), np.float64))
-        list_types.append(('output - control '+str(i), np.float64))
+    for i in controls_functions:
+        list_types.append(('in - '+str(i), np.float64))
+        list_types.append(('out - '+str(i), np.float64))
     result = np.array(rows, dtype=np.dtype(list_types))
 
     # plot the results
